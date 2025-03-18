08be3966029e0c9be5717b600d25b4558489afd9
vickitah <victor.mutuma@student.moringaschool.com>
2025-03-18 13:38:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // 62
+
+const random = Math.floor(Math.random() * 100) + 1; // Random integer > 0
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; // 4
+
+const max = Math.max(10, 5, 20, 15); // 20
